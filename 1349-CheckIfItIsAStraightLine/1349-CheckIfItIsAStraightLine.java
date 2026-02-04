3c606c9c2ba96fc3b4c1ee515944177b86e184e5
ragulperiyasamy24 <ragulperiyasamy24@gmail.com>
2026-02-04 22:45:50 +0530

    Create 1349-CheckIfItIsAStraightLine

---
+++ 1349-CheckIfItIsAStraightLine/1349-CheckIfItIsAStraightLine.java
@@ -0,0 +1,17 @@
+// Last updated: 2/4/2026, 10:45:51 PM
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+
+        int x0=coordinates[0][0] ;
+        int y0=coordinates[0][1];
+        int x1=coordinates[1][0] ;
+         int y1=coordinates[1][1];
+       for(int i=2;i<coordinates.length;i++){
+          int x=coordinates[i][0] , y=coordinates[i][1];
+          if((y1-y0)*(x-x0)!=(y-y0)*(x1-x0)){
+            return false;
+          }
+       } 
+       return true;
+    }
+}
